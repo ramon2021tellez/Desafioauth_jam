ed85b7fe335120eb0dc26d5379ab54243418ce4b
ramon2021tellez <miranda.tellez@gmail.com>
2022-06-10 01:10:29 -0400

    advance four

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController
   def new
-    
+
   end
 
   def create
+     user = User.find_by_email(params[:email])
+     if user && user.authenticate(params[:password])
+      session[:user_id] = user.index
+      redirect_to root_path, notice: 'logeado correctamente'
+     else
+      flash.now[:notice] = 'email o password invalida'
+      render action: :new
+     end
+
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'signed out successfully'
   end
 end
