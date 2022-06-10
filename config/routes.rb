361470a3b15ba5940d820cf474bde11ab682dedd
ramon2021tellez <miranda.tellez@gmail.com>
2022-06-09 23:20:12 -0400

    advance authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
- resources :users, only: [:new, :create, :show]
+ resources :sessions , only: [:new, :create, :destroy]
   # Defines the root path route ("/")
   root "home#index"
   
